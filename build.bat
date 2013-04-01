243c886e3603d64760b55bac7bdf33cd50746a89
Daniel <daniel@mester.se>
2013-04-01 20:12:35 +0200

    Added tests to output.

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ copy /Y stringformat.js Release\stringformat.src.js
 copy /Y license.txt Release\
 copy /Y readme.txt Release\
 copy /Y changelog.txt Release\
+copy /Y tests.html Release\
 
 rpc -encoding UTF-8 -q stringformat.js -header header.txt -o Release\stringformat.js -p "Daniel Mester Pirttij„rvi" --DEBUG=false
 
