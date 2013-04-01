bdb8f63c28862be0b6d0473489ceef2d121089ae
Daniel <daniel@mester.se>
2013-04-01 20:10:16 +0200

    Added changelog

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ del /Q Release\*.*
 copy /Y stringformat.js Release\stringformat.src.js
 copy /Y license.txt Release\
 copy /Y readme.txt Release\
+copy /Y changelog.txt Release\
 
 rpc -encoding UTF-8 -q stringformat.js -header header.txt -o Release\stringformat.js -p "Daniel Mester Pirttij„rvi" --DEBUG=false
 
