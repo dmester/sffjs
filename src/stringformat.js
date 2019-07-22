bf196967a463c3b6055437cec48d4ed8f09e3b1f
dmester <daniel@mester.se>
2019-07-22 17:05:04 +0200

    Formatting

--- src/stringformat.js
+++ src/stringformat.js
@@ -213,7 +213,7 @@ var sffjs = (function() {
             result = ensureFixedPoint(Number(result + "1").toFixed(decimals));
             
             // Trim excessive decimal zeroes
-            if (decimals > 0 ) {
+            if (decimals > 0) {
                 result = result.replace(/\.?0+$/, "");
             }
         }
